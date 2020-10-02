d4292d8c1e7132d9088656d8f8719d6a84383b73
faxe1008 <vielspeicherplatz6@gmail.com>
2020-10-02 17:43:41 +0000

    change stack keys

--- config.def.h
+++ config.def.h
@@ -58,8 +58,8 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 #define STACKKEYS(MOD,ACTION) \
-       { MOD, XK_j,     ACTION##stack, {.i = INC(+1) } }, \
-       { MOD, XK_k,     ACTION##stack, {.i = INC(-1) } }, \
+       { MOD, XK_k,     ACTION##stack, {.i = INC(+1) } }, \
+       { MOD, XK_j,     ACTION##stack, {.i = INC(-1) } }, \
        { MOD, XK_grave, ACTION##stack, {.i = PREVSEL } }, \
        { MOD, XK_q,     ACTION##stack, {.i = 0 } }, \
        { MOD, XK_a,     ACTION##stack, {.i = 1 } }, \
